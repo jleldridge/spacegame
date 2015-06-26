3b18b35a9b8534183ebb7051012ac7cf5c42da10
Luke Eldridge <jleldridge27@gmail.com>
2015-06-26 15:59:01 -0400

    Got http server working with websockets.

--- server.js
+++ server.js
@@ -1,185 +1,46 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
 var fs = require('fs');
 var WebSocketServer = require('ws').Server;
+var http = require('http');
+var app = express();
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000
+app.use(express.static(__dirname + '/public'));
 
-    //  Scope.
-    var self = this;
+// app.get('/', function(request, response) {
+//   var index = fs.readFileSync('./index.html');
+//   response.setHeader('Content-Type', 'text/html');
+//   response.send(index);
+// });
 
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running on port:" + app.get('port'))
+// })
+var server = http.createServer(app)
+server.listen(port)
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+i = 0;
+wss = new WebSocketServer({server: server});
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+wss.on('connection', function connection(ws) {
+  console.log('received %s', ws);
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+  ws.on('message', function incoming(message) {
+    var msg = JSON.parse(message);
+    var response = JSON.stringify({"messageType": "updatePlayer", "id": msg.id, "x": msg.x, "y": msg.y, "rotation": msg.rotation});
+    broadcast(response);
+  });
 
+  var clientMsg = {"messageType": "id", "id": i};
+  ws.send(JSON.stringify(clientMsg));
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+  var clientConnectedBroadcastMsg = {"messageType": "newPlayer", "id": i++};
+  broadcast(JSON.stringify(clientConnectedBroadcastMsg));
+});
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-
-        self.app.use(express.static('public'));
-    };
-
-    /**
-     * Initialize WebSocketsServer
-     */
-     self.initializeWebSocketServer = function() {
-       self.i = 0;
-       self.wss = new WebSocketServer({port: 81});
-
-       self.wss.on('connection', function connection(ws) {
-         console.log('received %s', ws);
-
-         ws.on('message', function incoming(message) {
-           var msg = JSON.parse(message);
-           var response = JSON.stringify({"messageType": "updatePlayer", "id": msg.id, "x": msg.x, "y": msg.y, "rotation": msg.rotation});
-           self.wss.broadcast(response);
-         });
-
-         var clientMsg = {"messageType": "id", "id": self.i};
-         ws.send(JSON.stringify(clientMsg));
-
-         var clientConnectedBroadcastMsg = {"messageType": "newPlayer", "id": self.i++};
-         self.wss.broadcast(JSON.stringify(clientConnectedBroadcastMsg));
-       });
-
-       self.wss.broadcast = function(data) {
-         self.wss.clients.forEach(function each(client) {
-           client.send(data);
-         });
-       }
-     };
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-        self.initializeWebSocketServer();
-    };
-
-};
-
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+broadcast = function(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+}
