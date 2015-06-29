614c0339bb6999c360906d53fb552cc37bcca545
Luke Eldridge <jleldridge27@gmail.com>
2015-06-29 09:25:59 -0400

    Added client disconnection handling.

--- server.js
+++ server.js
@@ -24,19 +24,28 @@ i = 0;
 wss = new WebSocketServer({server: server});
 
 wss.on('connection', function connection(ws) {
-  console.log('received %s', ws);
-
+  console.log('Client connected.');
+  
   ws.on('message', function incoming(message) {
     var msg = JSON.parse(message);
     var response = JSON.stringify({"messageType": "updatePlayer", "id": msg.id, "x": msg.x, "y": msg.y, "rotation": msg.rotation});
     broadcast(response);
   });
 
+  ws.on('close', function close(code, message) {
+    console.log('client closed with code %s\nmessage: %s', code, message);
+    var response = JSON.stringify({"messageType": "playerLeft", "id": ws.id});
+    broadcast(response);
+  });
+
   var clientMsg = {"messageType": "id", "id": i};
   ws.send(JSON.stringify(clientMsg));
 
-  var clientConnectedBroadcastMsg = {"messageType": "newPlayer", "id": i++};
+  ws.id = i;
+  var clientConnectedBroadcastMsg = {"messageType": "newPlayer", "id": i};
   broadcast(JSON.stringify(clientConnectedBroadcastMsg));
+
+  i++;
 });
 
 broadcast = function(data) {
